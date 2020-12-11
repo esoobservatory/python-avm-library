a553cacd631af71861a3b481138f7e7e60a14c16
Edison Arango <edisonarab@gmail.com>
2020-12-11 00:37:21 -0500

    Upgrade/disable broken schema

--- libavm/libavm/specs.py
+++ libavm/libavm/specs.py
@@ -44,7 +44,14 @@ AVM_SCHEMAS = {
     XMP_NS_IPTCCore: 'Iptc4xmpCore',
     XMP_NS_DC: 'dc',
     XMP_NS_AVM: 'avm',
-    XMP_NS_XMP: 'xmpRights',
+	#####
+	# Disabled when upgrading to Python 3 because it causes:
+	# libxmp.XMPError: Exempi function failure ("unknown error")
+	# When:
+	# xmp = libxmp.XMPMeta()
+	# xmp.register_namespace(XMP_NS_XMP, 'xmpRights')
+	#####
+    # XMP_NS_XMP: 'xmpRights',
     XMP_NS_Photoshop: 'photoshop',
 }
 
